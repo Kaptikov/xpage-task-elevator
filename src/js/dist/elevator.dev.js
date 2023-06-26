9d10102661b2796eef348185746139a0e7198ebb
Kaptikov <igor.zabirow02@mail.ru>
2023-06-26 11:27:08 +0500

    fix

--- src/js/dist/elevator.dev.js
+++ src/js/dist/elevator.dev.js
@@ -5,78 +5,105 @@ var firstFloor = document.querySelector('.floor__btn--first');
 var doorLeft = document.querySelector('.elevator__door--left');
 var doorRight = document.querySelector('.elevator__door--right');
 var elevator = document.querySelector('#elevator');
-var clickedBtns = [];
+var upQueue = [];
+var downQueue = [];
 var firstFloorBtn = firstFloor.getBoundingClientRect().top + window.pageYOffset;
 console.log(firstFloorBtn);
-var isMovingToFirstFloor = false;
+var isMovingDown = false; // elevator.style.top = `85.50%`
+
 floorBtns.forEach(function (btn) {
   btn.addEventListener('click', function () {
-    if (btn.classList.contains('floor__btn--active') || isMovingToFirstFloor) {
+    if (btn.classList.contains('floor__btn--active')) {
       return;
     }
 
+    var btnY = btn.getBoundingClientRect().top + window.pageYOffset;
     floorBtns.forEach(function (btn) {
       return btn.classList.remove('floor__btn--active');
     });
-    clickedBtns.push(btn);
-    btn.classList.add('floor__btn--next');
-    console.log(clickedBtns);
 
-    if (clickedBtns.length === 1) {
-      elevatorMovement();
+    if (elevator.style.top === '' || parseFloat(elevator.style.top) < btnY) {
+      downQueue.push(btn);
+    } else {
+      upQueue.push(btn);
+    }
+
+    btn.classList.add('floor__btn--next');
+    console.log(btnY);
+
+    if (upQueue.length === 1 && downQueue.length === 0 && isMovingDown === false) {
+      elevatorMovement(upQueue);
+    } else if (downQueue.length === 1 && upQueue.length === 0) {
+      elevatorMovement(downQueue);
     }
   });
 });
 
-function elevatorMovement() {
-  if (clickedBtns.length === 0) {
+function openDoors() {
+  doorLeft.style.width = "5%";
+  doorRight.style.width = "5%";
+}
+
+function closeDoors() {
+  doorLeft.style.width = "50%";
+  doorRight.style.width = "50%";
+}
+
+function moveElevator(btnY) {
+  elevator.style.top = "".concat(btnY, "px");
+  elevator.style.transform = "translate(-50%, -19.75%)";
+}
+
+function elevatorMovement(queue) {
+  if (queue.length === 0) {
     return;
   }
 
-  var currentBtn = clickedBtns[0];
+  var currentBtn = queue[0];
   var btnY = currentBtn.getBoundingClientRect().top + window.pageYOffset;
-  console.log(btnY);
   currentBtn.classList.remove('floor__btn--active');
+  console.log('Класс floor__btn--active удален');
   currentBtn.classList.add('floor__btn--active');
   currentBtn.classList.remove('floor__btn--next');
-  console.log('удален class next');
-  elevator.style.top = "".concat(btnY, "px");
-  elevator.style.transform = "translate(-50%, -19.75%)";
+  moveElevator(btnY);
   elevator.addEventListener('transitionend', function () {
-    doorLeft.style.width = "5%";
-    doorRight.style.width = "5%";
+    openDoors();
     doorLeft.addEventListener('transitionend', function () {
       currentBtn.classList.remove('floor__btn--active');
-      console.log('удален class active');
-      doorLeft.style.width = "50%";
-      doorRight.style.width = "50%";
+      console.log('Класс floor__btn--active удален');
+      closeDoors();
       doorLeft.addEventListener('transitionend', function () {
-        clickedBtns.shift();
-        console.log('удален первый элемент массива');
-        console.log(clickedBtns);
+        queue.shift();
+        console.log('удален первый элемент массива'); // currentBtn.classList.remove('floor__btn--next')
 
-        if (clickedBtns.length > 0) {
-          elevatorMovement();
+        console.log(queue);
+
+        if (queue.length > 0) {
+          elevatorMovement(queue);
         } else {
-          isMovingToFirstFloor = true;
-          console.log(isMovingToFirstFloor);
-          elevator.style.top = "".concat(firstFloorBtn, "px");
-          elevator.style.transform = "translate(-50%, -19.75%)";
-          firstFloor.classList.add('floor__btn--active');
-          doorLeft.style.width = "5%";
-          doorRight.style.width = "5%";
-          doorLeft.addEventListener('transitionend', function () {
-            doorLeft.style.width = "50%";
-            doorRight.style.width = "50%";
-            doorLeft.addEventListener('transitionend', function () {
-              isMovingToFirstFloor = false;
-              console.log(isMovingToFirstFloor);
+          if (queue === downQueue && upQueue.length > 0 || upQueue.length === 0 && downQueue.length === 0) {
+            isMovingDown = true;
+            moveElevator(firstFloorBtn);
+            console.log('Лифт вернулся на первый этаж');
+            elevator.addEventListener('transitionend', function () {
+              openDoors();
+              doorLeft.addEventListener('transitionend', function () {
+                isMovingDown = false;
+                closeDoors();
+                doorLeft.addEventListener('transitionend', function () {
+                  elevatorMovement(upQueue);
+                }, {
+                  once: true
+                });
+              }, {
+                once: true
+              });
             }, {
               once: true
             });
-          }, {
-            once: true
-          });
+          } else if (queue === upQueue && downQueue.length > 0) {
+            elevatorMovement(downQueue);
+          }
         }
       }, {
         once: true
--- src/js/elevator.js
+++ src/js/elevator.js
@@ -5,94 +5,136 @@ const doorLeft = document.querySelector('.elevator__door--left')
 const doorRight = document.querySelector('.elevator__door--right')
 
 const elevator = document.querySelector('#elevator')
-const clickedBtns = []
+const upQueue = []
+const downQueue = []
 
 const firstFloorBtn =
   firstFloor.getBoundingClientRect().top + window.pageYOffset
 console.log(firstFloorBtn)
 
-let isMovingToFirstFloor = false
+let isMovingDown = false
+
+// elevator.style.top = `85.50%`
 
 floorBtns.forEach(btn => {
   btn.addEventListener('click', () => {
-    if (btn.classList.contains('floor__btn--active') || isMovingToFirstFloor) {
+    if (btn.classList.contains('floor__btn--active')) {
       return
     }
 
+    const btnY = btn.getBoundingClientRect().top + window.pageYOffset
+
     floorBtns.forEach(btn => btn.classList.remove('floor__btn--active'))
 
-    clickedBtns.push(btn)
-    btn.classList.add('floor__btn--next')
-    console.log(clickedBtns)
+    if (elevator.style.top === '' || parseFloat(elevator.style.top) < btnY) {
+      downQueue.push(btn)
+    } else {
+      upQueue.push(btn)
+    }
 
-    if (clickedBtns.length === 1) {
-      elevatorMovement()
+    btn.classList.add('floor__btn--next')
+
+    console.log(btnY)
+
+    if (
+      upQueue.length === 1 &&
+      downQueue.length === 0 &&
+      isMovingDown === false
+    ) {
+      elevatorMovement(upQueue)
+    } else if (downQueue.length === 1 && upQueue.length === 0) {
+      elevatorMovement(downQueue)
     }
   })
 })
 
-function elevatorMovement() {
-  if (clickedBtns.length === 0) {
+function openDoors() {
+  doorLeft.style.width = `5%`
+  doorRight.style.width = `5%`
+}
+
+function closeDoors() {
+  doorLeft.style.width = `50%`
+  doorRight.style.width = `50%`
+}
+
+function moveElevator(btnY) {
+  elevator.style.top = `${btnY}px`
+  elevator.style.transform = `translate(-50%, -19.75%)`
+}
+
+function elevatorMovement(queue) {
+  if (queue.length === 0) {
     return
   }
 
-  const currentBtn = clickedBtns[0]
+  const currentBtn = queue[0]
   const btnY = currentBtn.getBoundingClientRect().top + window.pageYOffset
-  console.log(btnY)
 
   currentBtn.classList.remove('floor__btn--active')
+  console.log('Класс floor__btn--active удален')
   currentBtn.classList.add('floor__btn--active')
   currentBtn.classList.remove('floor__btn--next')
-  console.log('удален class next')
-  elevator.style.top = `${btnY}px`
-  elevator.style.transform = `translate(-50%, -19.75%)`
+  moveElevator(btnY)
+
   elevator.addEventListener(
     'transitionend',
     () => {
-      doorLeft.style.width = `5%`
-      doorRight.style.width = `5%`
+      openDoors()
 
       doorLeft.addEventListener(
         'transitionend',
         () => {
           currentBtn.classList.remove('floor__btn--active')
-          console.log('удален class active')
-          doorLeft.style.width = `50%`
-          doorRight.style.width = `50%`
+          console.log('Класс floor__btn--active удален')
+          closeDoors()
 
           doorLeft.addEventListener(
             'transitionend',
             () => {
-              clickedBtns.shift()
+              queue.shift()
               console.log('удален первый элемент массива')
-              console.log(clickedBtns)
 
-              if (clickedBtns.length > 0) {
-                elevatorMovement()
+              // currentBtn.classList.remove('floor__btn--next')
+              console.log(queue)
+
+              if (queue.length > 0) {
+                elevatorMovement(queue)
               } else {
-                isMovingToFirstFloor = true
-                console.log(isMovingToFirstFloor)
-                elevator.style.top = `${firstFloorBtn}px`
-                elevator.style.transform = `translate(-50%, -19.75%)`
-                firstFloor.classList.add('floor__btn--active')
-                doorLeft.style.width = `5%`
-                doorRight.style.width = `5%`
-                doorLeft.addEventListener(
-                  'transitionend',
-                  () => {
-                    doorLeft.style.width = `50%`
-                    doorRight.style.width = `50%`
-                    doorLeft.addEventListener(
-                      'transitionend',
-                      () => {
-                        isMovingToFirstFloor = false
-                        console.log(isMovingToFirstFloor)
-                      },
-                      { once: true }
-                    )
-                  },
-                  { once: true }
-                )
+                if (
+                  (queue === downQueue && upQueue.length > 0) ||
+                  (upQueue.length === 0 && downQueue.length === 0)
+                ) {
+                  isMovingDown = true
+                  moveElevator(firstFloorBtn)
+                  console.log('Лифт вернулся на первый этаж')
+
+                  elevator.addEventListener(
+                    'transitionend',
+                    () => {
+                      openDoors()
+
+                      doorLeft.addEventListener(
+                        'transitionend',
+                        () => {
+                          isMovingDown = false
+                          closeDoors()
+                          doorLeft.addEventListener(
+                            'transitionend',
+                            () => {
+                              elevatorMovement(upQueue)
+                            },
+                            { once: true }
+                          )
+                        },
+                        { once: true }
+                      )
+                    },
+                    { once: true }
+                  )
+                } else if (queue === upQueue && downQueue.length > 0) {
+                  elevatorMovement(downQueue)
+                }
               }
             },
             { once: true }
