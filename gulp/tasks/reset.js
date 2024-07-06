152e71c3745d9223f6be821e509d16b788ca5189
Kaptikov <igor.zabirow02@mail.ru>
2024-07-06 18:27:28 +0500

    create new project

--- gulp/tasks/reset.js
+++
@@ -1,9 +0,0 @@
-// import del from "del";
-// export const reset = () => {
-//     return del(app.path.clean);
-// }
-
-import { deleteAsync } from 'del'
-export const reset = () => {
-  return deleteAsync([app.path.clean])
-}
