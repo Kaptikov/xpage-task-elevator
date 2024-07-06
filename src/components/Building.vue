96d648f7114972ce1edbd5736b6d3b22319fc45c
Kaptikov <igor.zabirow02@mail.ru>
2024-07-06 19:43:23 +0500

    add base styles

---
+++ src/components/Building.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="building">
+    <h1 class="building__title">Xpage Task Elevator</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.building {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  // building__title
+  &__title {
+    margin: auto;
+    font-size: clamp(1.5rem, 0.955rem + 2.73vw, 3rem);
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+import './assets/styles/main.scss'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
